371969fe42b64f599ee7678da01b41b5e70b8338
yeongseok-yun <antiraveg2@gmail.com>
2022-04-28 16:27:35 +0900

    뷰 모달,이미지 삽입 예제

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <template>
+
+  <div class="black-bg" v-if="modalFlg == 1">
+    <div class = "white-bg">
+      <h4>상세페이지</h4>
+      <p>상세 내용</p>
+      <p @click = "modalFlg = 0">닫기</p>
+    </div>
+  </div>
+
   <div class = "menu">
     <!-- <a v-for="(menu,i) in menuList" :key="i">{{menuList[i]}}</a> -->
     <a v-for="menu in menuList" :key="menu">{{menu}}</a>
@@ -6,6 +15,7 @@
   </div>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <div v-for="(value,i) in valueList" :key="i">
+    <img @click= "modalFlg=1" src="./assets/img01.jpg" class= "room-img">
     <h4 :style = "style">{{products[i]}}</h4>
     <p>{{valueList[i]}} 만원</p>
     <button @click="increase(i)">허위매물신고</button><span>신고수 : {{faultCntList[i]}}</span>
@@ -20,6 +30,7 @@ export default {
   name: 'App',
   data() {
     return {
+      modalFlg : 0,
       logo : '원룸샵',
       style : 'color : blue',
       products : ['역삼동원룸','낙성대원룸','마포구원룸'],
@@ -47,6 +58,25 @@ export default {
   color: #2c3e50;
   /* margin-top: 60px; */
 }
+body{
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 .menu {
   background: darkslateblue;
   padding : 15px;
@@ -55,5 +85,9 @@ export default {
 .menu a {
   color : white;
   padding: 10px;
+}
+.room-img{
+  width:150px;
+  margin-top:20px;
 }
 </style>
