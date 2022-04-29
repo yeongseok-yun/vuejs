901ab692d28ccfea76f8038607bd880841c0fa9a
yeongseok-yun <antiraveg2@gmail.com>
2022-04-29 11:23:49 +0900

    뷰 v-model 입력 관련 예제

--- src/components/DetailModal.vue
+++ src/components/DetailModal.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="black-bg" v-if="modalFlg == 1">
     <div class = "white-bg">
+      <img class = "room-img" :src = "products[clickedNum].image">
       <h4>{{products[clickedNum].title}}</h4>
       <p>{{products[clickedNum].content}}</p>
-      <img class = "room-img" :src = "products[clickedNum].image">
-      <p>{{products[clickedNum].price}}원</p>
+      <!-- <input @input = "months = $event.target.value"> -->
+      <input v-model="months">
+      <p>{{months}}개월 선택함</p><p>{{months * products[clickedNum].price}}원</p>
       <DiscountCom/>
       <p @click = "$emit('closeModal')">닫기</p>
     </div>
@@ -18,7 +20,12 @@ export default {
         products : Array,
         clickedNum : Number,
         modalFlg : Number
-    }
+    },
+    data() {
+      return {
+        months: 1,
+      };
+    },
 }
 </script>
 
